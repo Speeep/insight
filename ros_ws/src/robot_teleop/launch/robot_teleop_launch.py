04f7103d84dbd535224cd474720afa4ac036b3b5
Giovanni <speeeep@gmail.com>
2025-09-04 14:19:25 -0400

    added image viewer to robot teleop launch file

--- ros_ws/src/robot_teleop/launch/robot_teleop_launch.py
+++ ros_ws/src/robot_teleop/launch/robot_teleop_launch.py
@@ -3,12 +3,23 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
+        # Teleop node in xterm
         Node(
             package="robot_teleop",
             executable="wasdqe_teleop",
             name="wasdqe_teleop",
             output="screen",
             prefix="xterm -e",
-        )
+        ),
+        
+        # Image viewer node in xterm
+        Node(
+            package='robot_teleop',
+            executable='image_viewer',
+            name='image_viewer',
+            output='screen',
+            prefix='xterm -e'
+        ),
+
     ])
 
